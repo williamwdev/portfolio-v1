1eded6e043ef41544df26f11b85d0235785c5f0d
William Y Wong <wwong1987@gmail.com>
2019-05-18 23:31:56 -0700

    basic boilerplate for projects page and resume page

---
+++ src/routes/ProjectsPage/ProjectsPage.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "../ProjectsPage/ProjectsPage.css";
+
+export default class ProjectsPage extends Component {
+  render() {
+    return (
+      <div className="projectsPage-container">
+        <h1>Projects Page</h1>
+      </div>
+    );
+  }
+}
