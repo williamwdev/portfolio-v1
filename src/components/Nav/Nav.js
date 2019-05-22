ab6581577195e6ad54f1eb83107d5ad0b4784814
William Y Wong <wwong1987@gmail.com>
2019-05-21 21:35:47 -0700

    added logo and fixed nav bar

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,19 +1,36 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import '../Nav/Nav.css'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "../Nav/Nav.css";
+import ww from "../../images/ww.png";
 
 export default class Nav extends Component {
   render() {
     return (
-      <div className='nav'>
-      <nav role="navigation"> 
-        <ul className='nav-list'>
-          <li className='home-nav'><Link to="/">About</Link></li>
-          <li className='projects-nav'><Link to="/projects">Projects</Link></li>
-          <li className='resume-nav'><Link to='/resume'>Contact</Link></li>
-        </ul> 
-      </nav>
-    </div>
-    )
+      <div className="nav-container">
+        <nav role="navigation">
+          <div className="logo">
+            <Link to="/">
+              <img
+                src={ww}
+                alt="screenshot of the dashboard of armory squad"
+                className="nav-logo"
+                height="50px"
+              />
+            </Link>
+          </div>
+          <ul className="nav-list">
+            <li className="home-nav">
+              <Link to="/">About</Link>
+            </li>
+            <li className="projects-nav">
+              <Link to="/projects">Projects</Link>
+            </li>
+            <li className="resume-nav">
+              <Link to="/resume">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    );
   }
 }
