70b422ca4b56dc05b6deabb0dd5e8df0df3f2a7b
William Y Wong <wwong1987@gmail.com>
2019-05-21 22:43:38 -0700

    debugging responsive viewport issue

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,8 +5,8 @@ export default class Header extends Component {
   render() {
     return (
       <header role='banner' className='home-banner'>
-        <h1>Hello, I'm <span className='header-name'>William Wong.</span></h1>
-        <p>Full stack developer</p>
+        <h1>Hello, I'm <span className='banner-name'>William Wong.</span></h1>
+        <span className='banner-subtitle'>Full stack developer based in San Diego</span>
       </header>
     )
   }
--- src/routes/HomePage/HomePage.js
+++ src/routes/HomePage/HomePage.js
@@ -1,14 +1,19 @@
-import React, { Component } from 'react';
-import Header from '../../components/Header/Header';
-import About from '../../components/About/About';
+import React, { Component } from "react";
+import Header from "../../components/Header/Header";
+import About from "../../components/About/About";
+import Projects from "../../components/Projects/Projects";
+import '../HomePage/HomePage.css';
 
 export default class HomePage extends Component {
   render() {
     return (
-      <main className='homepage-container'>
+      <main className="homepage-container">
         <Header />
-        <About/>
+        <section className="project-container">
+          <Projects />
+        </section>
+        <About />
       </main>
-    )
+    );
   }
 }
