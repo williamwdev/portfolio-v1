ebd1e19f0352efeadd12992ea55ce8fab1e9bfa4
William Wong <wwong1987@gmail.com>
2019-09-04 21:05:12 -0700

    working on contactform component, initial form, basic css, and route set up

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,18 +5,20 @@ import HomePage from "../../routes/HomePage/HomePage";
 import ProjectsPage from "../../routes/ProjectsPage/ProjectsPage";
 import AboutPage from "../../routes/AboutPage/AboutPage";
 import Footer from "../Footer/Footer";
-import '../App/App.css'
+import ContactForm from "../../components/Contact/ContactForm";
+import "../App/App.css";
 
 export default class App extends Component {
   render() {
     return (
       <div className="App">
-      <Nav />
+        <Nav />
         <main role="main">
           <Switch>
             <Route exact path={"/"} component={HomePage} />
             <Route exact path={"/about"} component={AboutPage} />
             <Route exact path={"/projects"} component={ProjectsPage} />
+            <Route exact path={"/contact"} component={ContactForm} />
           </Switch>
         <Footer />
         </main>
