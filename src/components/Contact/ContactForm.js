37718f6c3dce33ddd2d8017257a59a41edd5987b
William Wong <wwong1987@gmail.com>
2019-09-04 23:52:21 -0700

    audit fixed and working on contact form with axios

--- src/components/Contact/ContactForm.js
+++ src/components/Contact/ContactForm.js
@@ -1,45 +1,68 @@
-import React from 'react';
+import React, { Component } from "react";
+import axios from "axios";
 
 import './ContactForm.css';
 
-const ContactForm = () => (
-  <form className="contact-form" method="POST" action="/contact">
-    <div className="form-field">
-      <label htmlFor="name">
-        <div className="label-content">Name:</div>
-        <input type="text" name="name" required />
-      </label>
-    </div>
-
-    <div className="form-field">
-      <label htmlFor="email">
-        <div className="label-content">Email:</div>
-        <input type="email" name="email" required />
-      </label>
-    </div>
-
-    <div className="form-field">
-      <label htmlFor="message">
-        <div className="label-content">Message:</div>
-        <textarea className="stretch" name="message" rows="5" required />
-      </label>
-    </div>
-
-    <button type="submit">Send</button>
-
-    <div>
-      { window.location.hash === '#success' &&
-        <div id="success">
-          <p>Your message has been sent!</p>
-        </div>
+class ContactForm extends Component {
+  handleSubmit(e) {
+    e.preventDefault();
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+    axios({
+      method: "POST",
+      url: "http://localhost:3002/send",
+      data: {
+        name: name,
+        email: email,
+        messsage: message
       }
-      { window.location.hash === '#error' &&
-        <div id="error">
-          <p>An error occured while submitting the form.</p>
-        </div>
+    }).then(response => {
+      if (response.data.msg === "success") {
+        alert("Message Sent.");
+        this.resetForm();
+      } else if (response.data.msg === "fail") {
+        alert("Message failed to send.");
       }
-    </div>
-  </form>
-)
+    });
+  }
+
+  resetForm() {
+    document.getElementById("contact-form").reset();
+  }
+
+  render() {
+    return (
+      <div className="col-sm-4 offset-sm-4">
+        <form
+          id="contact-form"
+          onSubmit={this.handleSubmit.bind(this)}
+          method="POST"
+        >
+          <div className="form-group">
+            <label for="name">Name</label>
+            <input type="text" className="form-control" id="name" />
+          </div>
+          <div className="form-group">
+            <label for="exampleInputEmail1">Email address</label>
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              aria-describedby="emailHelp"
+            />
+          </div>
+          <div className="form-group">
+            <label for="message">Message</label>
+            <textarea className="form-control" rows="5" id="message"></textarea>
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default ContactForm;
