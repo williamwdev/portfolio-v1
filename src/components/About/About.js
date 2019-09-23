35dcf958ebdcb99ae70c4dbe2ecf1e2fecc08660
William Wong <wwong1987@gmail.com>
2019-09-23 14:48:02 -0700

    added linebreak in about page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -11,29 +11,35 @@ export default class About extends Component {
           src={aboutme2}
           alt="screenshot of the dashboard of armory squad"
           className="about-ss"
-          id='grad-ss'
+          id="grad-ss"
         />
         <p className="about-me-intro">
-          Hi, I'm <span className="strong">William</span> and I'm a Full-Stack Engineer specializing in PostgresQL, Express.js, React.js, and Node.js (PERN) stack. Since building my first
-          gaming PC at the age of 12 in order to gain a competitive edge in
-          Counter-Strike, I have been fascinated by the world of computers. I
-          want to use web development as a platform to intermarry my background
-          in Biochemistry and Economics with my passion for technology.
+          Hi, I'm <span className="strong">William</span> and I'm a Full-Stack
+          Engineer specializing in PostgresQL, Express.js, React.js, and Node.js
+          (PERN) stack. Since building my first gaming PC at the age of 12 in
+          order to gain a competitive edge in Counter-Strike, I have been
+          fascinated by the world of computers. I want to use web development as
+          a platform to intermarry my background in Biochemistry and Economics
+          with my passion for technology.
+          <br />
         </p>
+        <hr className="linebreak" />
         <br />
+
         <img
           src={aboutme3}
           alt="screenshot of the dashboard of armory squad"
           className="about-ss"
-          id='pancake-ss'
+          id="pancake-ss"
         />
         <p className="about-me-interests">
-          When I am not on my computer, you can find me taking my dog on picturesque
-          adventures while satiating my hunger for good food wherever I go. When
-          I’m not outside, I can be found engaging in competitive gaming and
-          fantasy sports (specifically, basketball and football). I believe in
-          lifelong self-development, finding ways to maximize efficiency, and
-          following my passion for technology and the latest electronic trends.
+          When I am not on my computer, you can find me taking my dog on
+          picturesque adventures while satiating my hunger for good food
+          wherever I go. When I’m not outside, I can be found engaging in
+          competitive gaming and fantasy sports (specifically, basketball and
+          football). I believe in lifelong self-development, finding ways to
+          maximize efficiency, and following my passion for technology and the
+          latest electronic trends.
         </p>
       </section>
     );
