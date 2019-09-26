06672bc3a8a60f629ef99986c4b28b22738f0f8f
William Wong <wwong1987@gmail.com>
2019-09-26 14:56:54 -0700

    changed width sizes of content in about and projects [age

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,7 +3,7 @@ import "../Projects/Projects.css";
 import armorySS from "../../images/armorySS.JPG";
 import happydogSS from "../../images/happydogSS.jpg";
 import bookmarkapp from "../../images/bookmarkappSS.jpg";
-import spacedRep from '../../images/spacedRep.jpg';
+import spacedRep from "../../images/spacedRep.jpg";
 
 export default class Projects extends Component {
   render() {
@@ -11,6 +11,33 @@ export default class Projects extends Component {
       <div className="projects-container">
         {/* <h2>Projects</h2> */}
         <section className="armory-project">
+          <h2 className="project-name">Squad Armory</h2>
+          <p>
+            An app for gamers to find friends to play with in their favorite
+            online multiplayer game. Create your own squad and find your perfect
+            teammates! Whether you're looking for something more casual or more
+            competitive, you can be as general or as specific as you'd like when
+            creating a squad. Chat with your new squadmates within the app and
+            plan out your gaming session.{" "}
+          </p>
+          <hr className="linebreak" />
+          <span className="tech-stack">
+            <strong>Built with</strong>: HTML5, CSS3, React.js, Node.js,
+            Express.js, , JWT, PostgreSQL, Nodemailer, Mocha/Chai, Supertest,
+            Enzyme, Zeit NOW, Heroku and SocketIO{" "}
+          </span>
+          <a
+            href="https://github.com/thinkful-ei-armadillo/The-Armory-Client"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="github-link"
+          >
+            <i
+              className="fab fa-github fa-lg"
+              id="footer-github-icon"
+              alt="github"
+            />
+          </a>
           <a
             href="https://squad-armory.now.sh"
             target="_blank"
@@ -22,36 +49,35 @@ export default class Projects extends Component {
               className="project-ss"
             />
           </a>
-          <h2 className='project-name'>Squad Armory</h2>
-          <p>
-            An app for gamers to find friends to play with in their favorite
-            online multiplayer game. Create your own squad and find your perfect
-            teammates! Whether you're looking for something more casual or more
-            competitive, you can be as general or as specific as you'd like when
-            creating a squad. Chat with your new squadmates within the app and
-            plan out your gaming session.{" "}
-          </p>
-          <hr className="linebreak"/>
-          <span className="tech-stack">
-            <strong>Built with</strong>: HTML5, CSS3, React.js, Node.js, Express.js, , JWT, PostgreSQL, Nodemailer, Mocha/Chai, Supertest, Enzyme, Zeit NOW, Heroku and
-            SocketIO{" "}
-          </span>
-          <a
-          href="https://github.com/thinkful-ei-armadillo/The-Armory-Client"
-          target="_blank"
-          rel="noopener noreferrer"
-          className='github-link'
-        >
-          <i
-            className="fab fa-github fa-lg"
-            id="footer-github-icon"
-            alt="github"
-          />
-        </a>
         </section>
 
-                
         <section className="spacedrep-project">
+          <h2 className="project-name">Spaced Repetition Spanish</h2>
+          <p>
+            A full-stack app created to help a user learn Spanish vocabulary
+            through the use of the spaced repetition technique. Words that you
+            get wrong will be seen more frequently than words you get correct
+            until you master them all. Implemented using singly linked list and
+            completed following strict test specifications.
+          </p>
+          <hr className="linebreak" />
+          <span className="tech-stack">
+            <strong>Built with</strong>: HTML5, CSS3, React.js, React Context,
+            Node.js, Knex.js, Express.js, JWT, Cypress, Mocha/Chai, Zeit NOW,
+            Heroku and PostgreSQL{" "}
+          </span>
+          <a
+            href="https://github.com/dc5will/happydog-client"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="github-link"
+          >
+            <i
+              className="fab fa-github fa-lg"
+              id="footer-github-icon"
+              alt="github"
+            />
+          </a>
           <a
             href="https://spaced-repetition-client-will-hunter.dc5will.now.sh"
             target="_blank"
@@ -63,30 +89,35 @@ export default class Projects extends Component {
               className="project-ss"
             />
           </a>
-          <h2 className='project-name'>Spaced Repetition Spanish</h2>
-          <p>
-          A full-stack app created to help a user learn Spanish vocabulary through the use of the spaced repetition technique. Words that you get wrong will be seen more frequently than words you get correct until you master them all. Implemented using singly linked list and completed following strict test specifications.
-          </p>
-          <hr className="linebreak"/>
-          <span className="tech-stack">
-            <strong>Built with</strong>: HTML5, CSS3, React.js, React Context, Node.js, Knex.js, Express.js, JWT, Cypress, Mocha/Chai, Zeit NOW, Heroku and PostgreSQL{" "}
-          </span>
-          <a
-          href="https://github.com/dc5will/happydog-client"
-          target="_blank"
-          rel="noopener noreferrer"
-          className='github-link'
-        >
-          <i
-            className="fab fa-github fa-lg"
-            id="footer-github-icon"
-            alt="github"
-          />
-        </a>
         </section>
 
-
         <section className="happydog-project">
+          <h2 className="project-name">Happy Dog</h2>
+          <p>
+            Take the guesswork out of making sure your dogs are happy by keeping
+            track of all of their basic needs. Ensure someone in your family
+            takes care of your furry friend's basic needs and more. Sometimes
+            life is hectic and we're not sure if our furry family members have
+            been taken care of. Keep secured important notes and tasks for your
+            dog.
+          </p>
+          <hr className="linebreak" />
+          <span className="tech-stack">
+            <strong>Built with</strong>: HTML5, CSS3, React.js, Node.js,
+            Express.js, JWT, Mocha/Chai, Enzyme, Supertest, and PostgreSQL{" "}
+          </span>
+          <a
+            href="https://github.com/dc5will/happydog-client"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="github-link"
+          >
+            <i
+              className="fab fa-github fa-lg"
+              id="footer-github-icon"
+              alt="github"
+            />
+          </a>
           <a
             href="https://happydog-app.dc5will.now.sh"
             target="_blank"
@@ -98,34 +129,32 @@ export default class Projects extends Component {
               className="project-ss"
             />
           </a>
-          <h2 className='project-name'>Happy Dog</h2>
-          <p>
-            Take the guesswork out of making sure your dogs are happy by keeping
-            track of all of their basic needs. Ensure someone in your family
-            takes care of your furry friend's basic needs and more. Sometimes
-            life is hectic and we're not sure if our furry family members have
-            been taken care of. Keep secured important notes and tasks for your
-            dog.
-          </p>
-          <hr className="linebreak"/>
-          <span className="tech-stack">
-            <strong>Built with</strong>: HTML5, CSS3, React.js, Node.js, Express.js, JWT, Mocha/Chai, Enzyme, Supertest, and PostgreSQL{" "}
-          </span>
-          <a
-          href="https://github.com/dc5will/happydog-client"
-          target="_blank"
-          rel="noopener noreferrer"
-          className='github-link'
-        >
-          <i
-            className="fab fa-github fa-lg"
-            id="footer-github-icon"
-            alt="github"
-          />
-        </a>
         </section>
 
         <section className="bookmarkapp-project">
+          <h2 className="project-name">Bookmark App</h2>
+          <p>
+            This is an web application that allows the users to save/rate their
+            favorite bookmarks. The app was bulit so I could have a way to
+            consolidate and rate all the sites I frequently visit.{" "}
+          </p>
+          <hr className="linebreak" />
+          <span className="tech-stack">
+            <strong>Built with</strong>: HTML, CSS, jQuery, JavaScript, and
+            RESTful API{" "}
+          </span>
+          <a
+            href="https://github.com/thinkful-ei-armadillo/will-matt-bookmark"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="github-link"
+          >
+            <i
+              className="fab fa-github fa-lg"
+              id="footer-github-icon"
+              alt="github"
+            />
+          </a>
           <a
             href="https://thinkful-ei-armadillo.github.io/will-matt-bookmark/"
             target="_blank"
@@ -138,28 +167,6 @@ export default class Projects extends Component {
             />
           </a>
           {/* <br/> */}
-          <h2 className='project-name'>Bookmark App</h2>
-          <p>
-            This is an web application that allows the users to save/rate their
-            favorite bookmarks. The app was bulit so I could have a way to
-            consolidate and rate all the sites I frequently visit.{" "}
-          </p>
-          <hr className="linebreak"/>
-          <span className="tech-stack">
-            <strong>Built with</strong>: HTML, CSS, jQuery, JavaScript, and RESTful API {" "}
-          </span>
-          <a
-          href="https://github.com/thinkful-ei-armadillo/will-matt-bookmark"
-          target="_blank"
-          rel="noopener noreferrer"
-          className='github-link'
-        >
-          <i
-            className="fab fa-github fa-lg"
-            id="footer-github-icon"
-            alt="github"
-          />
-        </a>
         </section>
       </div>
     );
